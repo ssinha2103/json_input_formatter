f518a8d402e1e7b5eef8740b063692acbd3649fa
ssinha2103 <s.sinha2103@outlook.com>
2024-07-18 02:04:20 +0530

    added the feature of DRF Home page to the app

--- json_formatter/views.py
+++ json_formatter/views.py
@@ -1,18 +1,23 @@
+from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.decorators import action
 from .serializers import JsonRequestSerializer, JsonResponseSerializer
 import json
 
 
-class JsonFormatView(APIView):
-    def post(self, request):
+class JsonFormatViewSet(viewsets.ViewSet):
+    @staticmethod
+    def list(request):
+        return Response({"message": "GET request received"})
+
+    @staticmethod
+    def create(request):
         serializer = JsonRequestSerializer(data=request.data)
         if serializer.is_valid():
-            data = serializer.validated_data['data']
+            data = serializer.validated_data.get('data', {})
             json_str = json.dumps(data, indent=4)
             formatted_string = json_str.replace('\n', '\r\n').replace('"', '\"')
-            formatted_string = '{' + formatted_string[1:-1] + '}'
             response_serializer = JsonResponseSerializer(data={'formatted_data': formatted_string})
             if response_serializer.is_valid():
                 return Response(response_serializer.data)
-        return Response(serializer.errors, status=400)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- json_input_formatter/settings.py
+++ json_input_formatter/settings.py
@@ -42,8 +42,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'formatter',
+    'json_formatter',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
