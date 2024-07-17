18f233a18e5b6da9471d724e7e93b567b0731c1a
ssinha2103 <s.sinha2103@outlook.com>
2024-07-17 22:22:59 +0530

    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Stop and remove existing containers
+docker stop web
+docker stop db
+docker rm web
+docker rm db
+
+# Build the Docker image
+docker build -t json_input_formatter .
+
+# Run the PostgreSQL container
+docker run -d --name db -e POSTGRES_DB=django -e POSTGRES_USER=django -e POSTGRES_PASSWORD=django -v postgres_data:/var/lib/postgresql/data postgres:13
+
+# Run the Django application container
+docker run -d -p 8000:8000 --name web --link db:db -e DATABASE_URL=postgres://django:django@db:5432/django json_input_formatter
